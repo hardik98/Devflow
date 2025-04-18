d98d149cb3b468977555ec391b2d3d7791a210f8
Hardik Halani <hthakkar7298@gmail.com>
2025-04-18 18:29:01 +0530

    Assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className="h1-bold">Welcome to the next js !!</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the next js !!</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to the next js !!</h1>
+    </>
+  );
 }
