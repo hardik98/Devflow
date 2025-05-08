7947a299556ac6d35bba445209628d9baaae9346
Hardik Halani <hthakkar7298@gmail.com>
2025-05-08 11:19:54 +0530

    Increment question details views

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 // tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"), // ✅ Add this line
+  ],
 };
